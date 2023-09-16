3690afb33d56f827339cfeb2510295d433eabc13
Fred Ngo <fredngo@gmail.com>
2023-09-16 09:13:23 -0400

    Step 2: Set up the Express server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+
+app.use('/public', express.static('./public'))
+app.use('/scripts', express.static('./public/scripts'))
+
+app.get('/', (req, res) => {
+  res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
+})
+
+const PORT = process.env.PORT || 3001
+    
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on http://localhost:${PORT}`)
+})
