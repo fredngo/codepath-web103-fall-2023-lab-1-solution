44fb0bd5bac59c8f8d91c434cfdd5aa0786d8497
Fred Ngo <fredngo@gmail.com>
2023-09-16 10:27:57 -0400

    Step 9: Dynamically display gift details

---
+++ client/public/scripts/gift.js
@@ -0,0 +1,29 @@
+const renderGift = async () => {
+  const requestedID = parseInt(window.location.href.split('/').pop())
+
+  const response = await fetch('/gifts')
+  const data = await response.json()
+
+  const giftContent = document.getElementById('gift-content')
+
+  let gift
+
+  gift = data.find(gift => gift.id === requestedID)
+
+  if (gift) {
+    document.getElementById('image').src = gift.image
+    document.getElementById('name').textContent = gift.name
+    document.getElementById('submittedBy').textContent = 'Submitted by: ' + gift.submittedBy
+    document.getElementById('pricePoint').textContent = 'Price: ' + gift.pricePoint
+    document.getElementById('audience').textContent = 'Great For: ' + gift.audience
+    document.getElementById('description').textContent = gift.description
+    document.title = `UnEarthed - ${gift.name}`
+  }
+  else {
+    const message = document.createElement('h2')
+    message.textContent = 'No Gifts Available 😞'
+    giftContent.appendChild(message)   
+  }
+}
+
+renderGift()
